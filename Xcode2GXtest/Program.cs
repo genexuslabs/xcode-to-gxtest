9821880015278f0648c2fdd584146fccb66930c6
Marcos Crispino <mcrispino@genexus.com>
2019-08-22 14:14:19 -0300

    Add project files.

---
+++ Xcode2GXtest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xcode2GXtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
